8ffce008919fab39905a9ff6c3455b6b983f58d1
Kerim Satirli <k.satirli@gmail.com>
2024-08-27 16:58:26 -0700

    cleans up outputs

--- outputs.tf
+++ outputs.tf
@@ -17,31 +17,31 @@ output "aws_workspace_urls" {
   ]
 }
 
-output "azure_locations" {
-  description = "Exported Values of JSON-encoded `./variables_azure_locations.json`."
-  value       = local.azure_locations
-}
+# output "azure_locations" {
+#   description = "Exported Values of JSON-encoded `./variables_azure_locations.json`."
+#   value       = local.azure_locations
+# }
 
-output "azure_workspace_urls" {
-  description = "Azure-specific Regional Workspace URLs."
+# output "azure_workspace_urls" {
+#   description = "Azure-specific Regional Workspace URLs."
+#
+#   value = [
+#     for region, workspace in module.regional_azure_workspaces : workspace.tfe_workspace.html_url
+#   ]
+# }
 
-  value = [
-    for region, workspace in module.regional_azure_workspaces : workspace.tfe_workspace.html_url
-  ]
-}
+# output "google_regions" {
+#   description = "Exported Values of `data.google_compute_regions.main`."
+#   value       = data.google_compute_regions.main
+# }
 
-output "google_regions" {
-  description = "Exported Values of `data.google_compute_regions.main`."
-  value       = data.google_compute_regions.main
-}
-
-output "google_workspace_urls" {
-  description = "Google Cloud-specific Regional Workspace URLs."
-
-  value = [
-    for region, workspace in module.regional_google_workspaces : workspace.tfe_workspace.html_url
-  ]
-}
+# output "google_workspace_urls" {
+#   description = "Google Cloud-specific Regional Workspace URLs."
+#
+#   value = [
+#     for region, workspace in module.regional_google_workspaces : workspace.tfe_workspace.html_url
+#   ]
+# }
 
 output "tfe_workspace_terraform_version" {
   description = "Terraform version identifier of current HCP Terraform Workspace."
